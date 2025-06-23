364a89e122d111da6ff1caf614ad86fa6683cdf2
gabriellbf <gabbriel.barbosa1@gmail.com>
2025-06-23 01:21:50 -0300

    otimizado

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//botões animados ao clicar
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink (link) {
+    link.addEventListener("click", function () {
+        links.forEach((item) => item.classList.remove("ativo"));
+        this.classList.add("ativo");
+    })
+}
+
+links.forEach(ativarLink);
+
+//FAQ
+
+const perguntas = document.querySelectorAll(".faq button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    const ativa = resposta.classList.contains("ativa")
+
+    if (ativa) {
+        resposta.style.maxHeight = null;
+        resposta.classList.remove("ativa");
+        pergunta.setAttribute('aria-expanded', false);
+    } else {
+        resposta.style.maxHeight = resposta.scrollHeight + "px";
+        resposta.classList.add("ativa");
+        pergunta.setAttribute('aria-expanded', true);
+    }
+}
+
+perguntas.forEach((pergunta) => {
+    pergunta.addEventListener('click', ativarPergunta);
+});
+
+//Animação
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
+
+
+
